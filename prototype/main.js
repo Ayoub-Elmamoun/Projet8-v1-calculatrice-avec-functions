ad1cb2b030a46b08d480c790244145c391974ea3
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-12-20 12:52:26 +0100

    added

--- prototype/main.js
+++ prototype/main.js
@@ -3,4 +3,9 @@ function calculer()
     let a = document.getElementById("output").value 
     let b = eval(a) 
     document.getElementById("output").value = b 
-} 
+}
+
+function afficher(val)
+{ 
+    document.getElementById("output").value+=val 
+}
