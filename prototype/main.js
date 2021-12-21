b2f37fcb63ccfcc385155af2ea351017502889e1
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-12-21 15:54:57 +0100

    added

--- prototype/main.js
+++ prototype/main.js
@@ -1,11 +1,1 @@
-function calculer() 
-{ 
-    let a = document.getElementById("output").value 
-    let b = eval(a) 
-    document.getElementById("output").value = b 
-}
-
-function afficher(val)
-{ 
-    document.getElementById("output").value+=val 
-}
+let PremierNombre, secondNumber , opration
