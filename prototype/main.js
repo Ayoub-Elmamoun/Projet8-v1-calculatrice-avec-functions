d968b420e139e8976bf2d3cec9165f40a81c1ce4
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-12-21 16:03:27 +0100

    added

--- prototype/main.js
+++ prototype/main.js
@@ -1,1 +1,20 @@
-let PremierNombre, secondNumber , opration
+
+// declaration des variables
+
+let PremierNombre, SecondNomber , operation , resultat ;
+let PremierNombreStr , SecondNumberStr ;
+
+//affichage 
+
+function ClickNombre(nombre)
+{
+    if (PremierNombre == undefined){       
+            PremierNombre = nombre
+        }
+    else
+        {
+            if (SecondNomber == undefined){
+                SecondNomber = nombre
+            }
+        }
+}
