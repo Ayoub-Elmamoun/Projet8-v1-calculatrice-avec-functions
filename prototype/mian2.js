d50a3710e69464961b79b102d0c5de0873aa2221
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-12-23 12:13:07 +0100

    added

---
+++ prototype/mian2.js
@@ -0,0 +1,49 @@
+
+// declaration des variables 
+
+let x,y,result,operation
+let xStr , yStr
+
+// traitement
+
+function ClickNumber(number)
+{
+   if (x == undefined) {
+        x=number
+   }
+   else {
+       if (y == undefined) {
+           y=number
+       }
+   }
+
+   affichage()
+}
+
+function affichage(number)
+{
+    let affichage=document.getElementById("affichage")
+    affichage.value = ""
+    if (x != undefined && y != undefined && operation != undefined) {
+        affichage.value = number
+    }
+    else
+    {
+
+    if (x != undefined) {
+        affichage.value += x        
+    }
+    if (y !=undefined) {
+        affichage.value +=y        
+    }
+    if (operation != undefined) {
+        affichage.value += operation        
+    }
+
+    }
+}
+
+function egale ()
+{
+    
+}
