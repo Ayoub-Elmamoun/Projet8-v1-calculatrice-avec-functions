f5368fdf6e1546ab10d876f4bfa2346496601691
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-12-23 15:09:02 +0100

    added

--- live-coding/main.js
+++ live-coding/main.js
@@ -1,2 +1,28 @@
 // declaration des variables
- let 
+
+let x,y,result,operation
+let xStr , yStr
+
+// traitement
+
+function ClickNum(number)
+{
+   if (x == undefined) {
+        x=number
+   }
+   else {
+       if (y == undefined) {
+           y=number
+       }
+   }
+
+   affichage()
+}
+
+function affichage(number)
+{
+    let affichage=document.getElementById("affichage")
+    affichage.value = ""
+    if (x != undefined && y != undefined && operation != undefined) {
+        affichage.value = number
+    }
